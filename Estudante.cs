c50668748a5f2f027edeb3663b7e0f5ec58c0262
Rahyan Ramos <rahyanramos.dev@gmail.com>
2023-11-13 20:24:53 -0400

    Criando Classe da lista de estudantes

---
+++ Estudante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoProva
+{
+    public class Estudante
+    {
+        string nome, cpf, curso, telefone, email;
+        string[,] disciplinas;
+        Estudante prox;
+    }
+}
