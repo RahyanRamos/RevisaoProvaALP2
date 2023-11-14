8a510b2299a5fd1080bd347462e2d0f4414d4733
Rahyan Ramos <rahyanramos.dev@gmail.com>
2023-11-13 20:12:35 -0400

    Apresentação dos valores do vetor

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ using static System.Convert;
 int[] numero = new int[10];
 int[][] vet = new int[10][];
 int[,] vet2 = new int[10,10];
+
+for (int i = 0; i < vet.GetLength(0); i++)
+{
+	for (int j = 0; j < vet.GetLength(1); j++)
+	{
+		WriteLine(vet[i][j]);
+	}
+}
+
